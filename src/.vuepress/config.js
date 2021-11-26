598a12bb85c4ea59549864266d902d94cd8f0f2e
zengyuanyuan <zengyuanyuan@tuhu.cn>
2021-11-26 21:12:17 +0800

    gitignore

---
+++ src/.vuepress/config.js
@@ -0,0 +1,10 @@
+/**
+ * @Desc:
+ * @Author: zengyuanyuan
+ * @Date: 2021/11/26
+ */
+const path = require('path')
+module.exports = {
+  title: 'pudding',
+  dest: path.resolve(__dirname, '../../docs')
+}
